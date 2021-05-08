4489a0fb77bf179e10bdb225502a9303d8e8f573
KalVasFlame <unnameddwar@gmail.com>
2021-05-08 15:48:14 +0300

    +

--- src/js/serviceApi.js
+++ src/js/serviceApi.js
@@ -1,3 +1,5 @@
+import countriesData from '../js/countriesDataList'
+
 const KEY = "c1xQ4GUaMnAePoI6UzGXPAXCsKa26y8D";
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/";
  
@@ -14,10 +16,43 @@ export default {
   },
   getEventById(id) {
     const url = `${BASE_URL}events/${id}.json?apikey=${KEY}`;
-   return fetch(url).then(r=>r.json())
+    return fetch(url).then(r => r.json())
   },
   getEventsByCountryName(countryName) {
-    const url = `${BASE_URL}events.json?page=${this.page}&size=${this.perPage}&countryCode=${co}apikey=${KEY}`
-  }
-}
+    let countryCode = '';
+    Object.entries(countriesData).forEach(i => {
+      if (i[1] === countryName) {
+        countryCode = i[0];
+        return;
+      }
+    })
+    const url = `${BASE_URL}events.json?page=${this.page}&size=${this.perPage}&countryCode=${countryCode}&apikey=${KEY}`;
+    return fetch(url).then(r => r.json()).then(r => {
+      const data = r._embedded.events
+      return data
+    });
+  },
+  getEventsBySearchQuery(searchQuery) {
+    const url = `${BASE_URL}events.json?keyword=${searchQuery}&apikey=${KEY}`;
+    return fetch(url).then(r => r.json()).then(r => {
+      const data = r._embedded.events
+      return data
+    });
+  },
+  incrementPage() {
+    this.page += 1;
+  },
+  decrementPage() {
+    this.page -= 1;
+  },
+  resetPage() {
+    this.page = 1;
+  },
+  changeSizeSetting(val) {
+    this.perPage = val;
+  },
+  resetSizeSettings() {
+    this.perPage = 20;
+  },
+};
 
