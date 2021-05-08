d14540e44f7d3b04ca9e1b36610a0409d7f31f1e
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-05-08 18:52:30 +0300

    added open-close modal fns

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+    gallery: document.querySelector('.card-list'),
+    btnModalClose: document.querySelector('.modal-close'),
+    ticketInfoContainer: document.querySelector('.ticket-info-container'),
+    ticketModal: document.querySelector('.backdrop'),
+}
