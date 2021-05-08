9091cc63271214d90742aab50506aae837ef4bac
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-08 12:17:19 +0300

    contiesList, api base

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,19 @@
 const KEY = "c1xQ4GUaMnAePoI6UzGXPAXCsKa26y8D";
-const BASE_URL = "https://app.ticketmaster.com/discovery/v2/";
+const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events";
 
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=lp&apikey=c1xQ4GUaMnAePoI6UzGXPAXCsKa26y8D").then(r=>r.json()).then(console.log);
+// countryCode
+// .json?
+// console.log(`${BASE_URL}.json?size=20&apikey=${KEY}`);
+
+// fetch(`${BASE_URL}.json?size=21&page=1&apikey=${KEY}`).then(r=>r.json()).then(console.log);
+// fetch(`${BASE_URL}.json?size=21&page=1&apikey=${KEY}`)
+//   .then(r => r.json())
+//   .then(r => r['_embedded'].events[0].id)
+//   .then(r => fetch(`${BASE_URL}/${r}.json?apikey=${KEY}`))
+//   .then(r => r.json())
+//   .then(console.log);
+const apiService = {
+  page: 1,
+  size: 20,
+  
+}
