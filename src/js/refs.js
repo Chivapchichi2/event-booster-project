2a8a7718ddabb7686f686e3f42df94b6a39f4d8a
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-05-09 20:25:49 +0300

    added murkup to modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,4 @@
-export default {
-    gallery: document.querySelector('.card-list'),
+export default {    
     btnModalClose: document.querySelector('.modal-close'),
     ticketInfoContainer: document.querySelector('.ticket-info-container'),
     ticketModal: document.querySelector('.backdrop'),
