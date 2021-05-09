a61fdcbcd81d5238d355cea49422c85b88929120
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-09 03:04:29 +0300

    Create classificationNameList.js

---
+++ src/js/classificationNameList.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: "KZFzniwnSyZfZ7v7nE",
+    name: "Sports",
+  },
+  {
+    id: "KZFzniwnSyZfZ7v7nJ",
+    name: "Music",
+  },
+  {
+    id: "KZFzniwnSyZfZ7v7na",
+    name: "Arts & Theatre",
+  },
+  {
+    id: "KZFzniwnSyZfZ7v7nn",
+    name: "Film",
+  },
+  {
+    id: "KZFzniwnSyZfZ7v7n1",
+    name: "Miscellaneous",
+  }
+];
