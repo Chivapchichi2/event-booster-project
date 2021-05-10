497e18858bc0c2f887a276bb4eb4886301127188
Irina Romjalis <iromjalis@gmail.com>
2021-05-11 00:07:34 +0200

    pagination

--- src/js/utils/serviceApi.js
+++ src/js/utils/serviceApi.js
@@ -1,51 +1,62 @@
-import countriesData from '../data/countriesDataList'
-import genresData from '../data/classificationNameList'
-const KEY = "c1xQ4GUaMnAePoI6UzGXPAXCsKa26y8D";
-const BASE_URL = "https://app.ticketmaster.com/discovery/v2/";
- 
+import countriesData from '../data/countriesDataList';
+import genresData from '../data/classificationNameList';
+const KEY = 'c1xQ4GUaMnAePoI6UzGXPAXCsKa26y8D';
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 
 export default {
   page: 1,
   perPage: 20,
+  galleryStatus: 'ByUpcoming',
   getWorldUpcomingEvents() {
+    this.galleryStatus = 'ByUpcoming';
     const url = `${BASE_URL}events.json?page=${this.page}&size=${this.perPage}&apikey=${KEY}`;
-    return fetch(url).then(r => r.json()).then(r => {
-      const data = r._embedded.events
-      return data
-    });
+    return fetch(url)
+      .then(r => r.json())
+      .then(r => {
+        const data = r._embedded.events;
+        return data;
+      });
   },
   getEventById(id) {
+    this.galleryStatus = 'ById';
+
     const url = `${BASE_URL}events/${id}.json?apikey=${KEY}`;
-    return fetch(url).then(r => r.json())
+    return fetch(url).then(r => r.json());
   },
   getEventsBySearchQuery(searchQuery) {
+    this.galleryStatus = 'BySearch';
+
     const url = `${BASE_URL}events.json?keyword=${searchQuery}&apikey=${KEY}`;
-    return fetch(url).then(r => r.json()).then(r => {
-      
-      const data = r._embedded.events
-      return data
-    });
+    return fetch(url)
+      .then(r => r.json())
+      .then(r => {
+        const data = r._embedded.events;
+        return data;
+      });
   },
   getEventsByFilter(genre, countryName) {
+    this.galleryStatus = 'ByFilter';
+
     let genreId = '';
     let countryCode = '';
     countriesData.forEach(i => {
       if (i.name === countryName) {
         countryCode = i.code;
-        
       }
-    })
+    });
     genresData.forEach(i => {
       if (i.name === genre) {
-        genreId = i.id
+        genreId = i.id;
       }
-    })
-    
+    });
+
     const url = `${BASE_URL}events.json?classificationId=${genreId}&countryCode=${countryCode}&apikey=${KEY}`;
-    return fetch(url).then(r => r.json()).then(r => {
-      const data = r._embedded?.events
-      return data
-    })
+    return fetch(url)
+      .then(r => r.json())
+      .then(r => {
+        const data = r._embedded?.events;
+        return data;
+      });
   },
   incrementPage() {
     this.page += 1;
@@ -63,4 +74,3 @@ export default {
     this.perPage = 20;
   },
 };
-
