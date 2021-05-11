329ca7da5a43ebd340ed00605f2432c55594b3ea
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-05-11 23:33:38 +0300

    ---

--- src/js/render/renderCardList.js
+++ src/js/render/renderCardList.js
@@ -3,8 +3,7 @@ import cardListHbs from '../../templates/card-list.hbs';
 import refs from '../utils/refs';
 import validation from '../utils/validation';
 
-apiService.getWorldUpcomingEvents().then(data => {
-  apiService.page = 1
+apiService.getWorldUpcomingEvents().then(data => {  
   validation.imageUrl(data);
   const markup = cardListHbs(data);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
