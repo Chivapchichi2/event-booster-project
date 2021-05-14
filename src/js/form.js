cdd1a944c3489a22be66cb66128493a7551daf40
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-15 00:05:50 +0300

    fix

--- src/js/form.js
+++ src/js/form.js
@@ -12,8 +12,8 @@ refs.form.addEventListener('click', e => {
   if (validation.checkTargetNodeName(e)) {
     validation.changeBtnText(e);
     apiService.resetPage();
-    apiService.countryCode = validation.transformCountriesNameIntoCode(refs.countryBtn.textContent, countriesData);
-    apiService.genresId = validation.transformGenreIntoId(refs.categoryBtn.textContent, genresData);
+    apiService.countryCode = validation.transformCountriesNameIntoCode(refs.countryBtn.textContent.trim(), countriesData);
+    apiService.genresId = validation.transformGenreIntoId(refs.categoryBtn.textContent.trim(), genresData);
     apiService.getEventsByFilter(apiService.genresId, apiService.countryCode)
       .then(data => {
         if (!data) {
