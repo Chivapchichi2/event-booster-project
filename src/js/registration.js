6b420477a5240a254960f39eef863bb554422e0f
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-18 12:49:20 +0300

    +

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,13 +1,11 @@
 import registration from './utils/firebaseApi';
-// import { openCloseModal } from './registration-helpers';
 import refs from './utils/refs';
 import 'firebaseui/dist/firebaseui.css';
 
 window.addEventListener('load', () => registration.initApp());
 
 refs.registrationBtn.addEventListener('click', () => {
-  console.log('22');
-  // if (refs.registrationBtn.firstElementChild.classList.contains('besom-broom')) return;
+  // if (refs.registrationBtn.firstElementChild === refs.iconIn) { }
 
   // openCloseModal();
   registration.start('#firebaseui-auth-container');
---
+++ src/js/utils/firebaseRegistratioHelpers.js
@@ -0,0 +1,8 @@
+import refs from './refs';
+
+export default {
+  signedUser(url, name) { console.log(url, name); },
+  noSignedUser() {console.log('no user');},
+}
+// refs.iconOut.style.display = 'none';
+// refs.firebaseContainer.style.display = 'none';
